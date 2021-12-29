8766e71bcc2d971a7448411ef360964539293625
KCrandall6 <kcrandall67@gmail.com>
2021-12-29 11:34:42 -0700

    Co-authored-by: KyleBoudewyn <kyleboudewyn@me.com> Co-authored-by: Lzzhengyi <Lzzhengyi@gmail.com> Co-authored-by: tmow12 <trevormow@gmail.com> Co-authored-by: AaronJGaut <aarongautmilk@gmail.com> created mongodb schema, worked on undo logic

--- echo-server/client/App.jsx
+++ echo-server/client/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const socket = new WebSocket("ws://localhost:3000");
 
-let message = 'hello, I clicked a button!';
+let message = "hello, I clicked a button!";
 
 socket.onerror = function (error) {
   alert(`[error] ${error.message}`);
@@ -11,6 +11,7 @@ socket.onerror = function (error) {
 function sendWebSocketMessage() {
   socket.send(message);
 }
+function sendWebSocketUndoMessage(){}
 //
 socket.onmessage = function (event) {
   console.log(`[message] Data received from server: ${event.data}`);
@@ -18,8 +19,13 @@ socket.onmessage = function (event) {
   if (event.data instanceof Blob) {
     const reader = new FileReader();
 
+    //reader.onload is invoked as a result of readAsText
     reader.onload = () => {
       console.log("Result: " + reader.result);
+      //convert reader.result from a string to an object
+      const readState = JSON.parse(reader.result);
+      //we need to set this object as the state for the client
+      document.getElementById('echoTextDisplay').value = readState;
     };
 
     reader.readAsText(event.data);
@@ -32,13 +38,19 @@ const App = () => {
   return (
     <div>
       <h1>Narcissus's Mirror</h1>
+      <textarea
+        className="textDisplay"
+        id="echoTextDisplay"
+        placeholder=""
+      ></textarea>
       <input
         className="textInput"
         id="echoText"
         placeholder="what would you have me say?"
-        onChange={(e) => message = (e.target.value)}
+        onChange={(e) => (message = e.target.value)}
       ></input>
       <button onClick={sendWebSocketMessage}>Send</button>
+      <button onClick={sendWebSocketUndoMessage}>Undo</button>
     </div>
   );
 };
