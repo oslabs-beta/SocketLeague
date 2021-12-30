6049e180f48434a646d87cfe11615f1aa2122015
KCrandall6 <kcrandall67@gmail.com>
2021-12-29 21:00:59 -0700

    added syncHandler Co-authored-by: KyleBoudewyn <kyleboudewyn@me.com> Co-authored-by: Lzzhengyi <Lzzhengyi@gmail.com> Co-authored-by: KCrandall6 <kcrandall67@gmail.com> Co-authored-by: tmow12 <trevormow@gmail.com> Co-authored-by: AaronJGaut <aarongautmilk@gmail.com>

--- echo-server/server/models/clientModel.js
+++ echo-server/server/models/clientModel.js
@@ -2,17 +2,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 require('dotenv').config();
 
-
-// const MONGO_URI = 
-//   'mongodb+srv://samflam:upBPnFCW1Gwmeo2hVpD7okpKeJmCfiICk1HzcRH7@cluster0.foabe.mongodb.net/SocketLeague?retryWrites=true&w=majority';
-
-mongoose
-  .connect(process.env.DB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((err) => console.log(err))
+// mongoose
+//   .connect(process.env.DB_URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => console.log('Connected to MongoDB'))
+//   .catch((err) => console.log(err))
 
 
   const clientSchema = new Schema({
