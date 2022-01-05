cbbc5ceb5535d85983468aa513cdf6834c4f1284
Kyle Boudewyn <kyleboudewyn@kyles-MacBook-Pro.local>
2022-01-05 13:42:00 -0800

    Updated echo-server user and style and syncHanler undo Co-authored-by: KyleBoudewyn <kyleboudewyn@me.com> Co-authored-by: Lzzhengyi <Lzzhengyi@gmail.com> Co-authored-by: KCrandall6 <kcrandall67@gmail.com> Co-authored-by: tmow12 <trevormow@gmail.com> Co-authored-by: AaronJGaut <aarongautmilk@gmail.com>

--- echo-server/client/App.jsx
+++ echo-server/client/App.jsx
@@ -1,83 +1,66 @@
 import React from 'react';
 import { useSyncState, Connection } from 'socket-league-client';
-
-//console.log(`Using same react instance? ${React === HookReact}`);
+import '../style.css';
 
 const socket = new WebSocket('ws://localhost:3000');
-
 let message = 'hello, I clicked a button!';
 let session_id = '0';
-// const newContent = {
-//   timestamp: new Date().toLocaleString(),
-//   message,
-//   user: 'anon',
-// };
 
 socket.onerror = function (error) {
   alert(`[error] ${error.message}`);
 };
 
+/**
+ * This function formats the message in an object
+ * called msgObject and sends it to the web socket
+ * @param socketState is the current state
+ */
 function sendWebSocketMessage(socketState) {
   console.log('We are in the send update websocket message function');
+  const username = document.getElementById('username').value;
   const msgObject = {};
   msgObject.state = [...socketState];
   const newContent = {
     timestamp: new Date().toLocaleString(),
     message,
-    user: 'anon',
+    user: username,
   };
   msgObject.state.push(newContent);
-  //msgObject.state.push(message);
   msgObject.action = 'update';
-  msgObject.session = session_id; //'0'
+  msgObject.session = session_id;
   console.log('we are about to attempt to send');
   conn._publish(msgObject);
   console.log('send successful');
+  document.getElementById('echoText').value = '';
 }
 
+/**
+ * This function will undo the most recent state change
+ * @param socketState is the current state
+ */
 function sendWebSocketUndoMessage(socketState) {
   console.log('We are in the send undo websocket message function');
+  const username = document.getElementById('username').value;
   const msgObject = {};
+  // are the below 4 lines needed for the undo function? I think all we need is action and session
   msgObject.state = [...socketState];
   msgObject.state.timestamp = new Date().toLocaleString();
   msgObject.state.message = message;
-  msgObject.state.user = 'anon';
+  msgObject.state.user = username;
   msgObject.action = 'undo';
-  msgObject.session = session_id; //'0'
+  msgObject.session = session_id;
   conn._publish(msgObject);
 }
 
-function joinSession0() {
-  console.log('Welcome to chat room 1');
-  session_id = '0';
+/**
+ * Function to handle session joining
+ * @param session This is the session id that is being joined
+ */
+function joinSession(session) {
+  console.log(`Welcome to chat room ${session}`);
+  session_id = session;
   //ADD LOGIC TO INITIALIZE SESSION
 }
-function joinSession1() {
-  console.log('Welcome to chat room 2');
-  session_id = '1';
-  //ADD LOGIC TO INITIALIZE SESSION
-}
-
-socket.onmessage = function (event) {
-  console.log(`[message] Data received from server: ${event.data}`);
-  //console.log(`[message] Data received from server: ${JSON.parse(event.data)}`);
-  if (event.data instanceof Blob) {
-    const reader = new FileReader();
-
-    //reader.onload is invoked as a result of readAsText
-    reader.onload = () => {
-      console.log('Result: ' + reader.result);
-      //convert reader.result from a string to an object
-      const readState = JSON.parse(reader.result);
-      //we need to set this object as the state for the client
-      console.log(readState);
-    };
-
-    reader.readAsText(event.data);
-  } else {
-    console.log('Result: ' + event.data);
-  }
-};
 
 const conn = new Connection('ws://localhost:3000');
 
@@ -91,39 +74,46 @@ const App = () => {
   const textMsg = [];
   for (let i = 0; i < socketState.length; i++) {
     textMsg.push(
-      `Time: ${socketState[i].timestamp} User: ${socketState[i].user} Message: ${socketState[i].message}`
+      `[${socketState[i].timestamp}] ${socketState[i].user}: ${socketState[i].message}`
     );
   }
   let session = session_id;
   console.log('socketstate is , ', socketState);
   console.log('textMsg is , ', textMsg);
   return (
-    <div>
-      <h1>Socket League Demo (Anonymous Chat Room)</h1>
-      <button onClick={() => joinSession0()}>Chat Room 1</button>
-      <button onClick={() => joinSession1()}>Chat Room 2</button>
-      <span>Current Session ID is {session_id}</span>
+    <div className="main-container">
+      <div className="title">
+        <h1>Socket League Demo (Chat Room)</h1>
+      </div>
+      <div className="sessionBtns">
+        <button onClick={() => joinSession(0)}>Chat Room 1</button>
+        <button onClick={() => joinSession(1)}>Chat Room 2</button>
+        <button onClick={() => joinSession(2)}>Chat Room 3</button>
+        <button onClick={() => joinSession(3)}>Chat Room 4</button>
+        <span>Session ID is {session_id}</span>
+      </div>
       <br></br>
-      <textarea
-        style={{ width: '500px', height: '300px' }}
-        className="textDisplay"
-        id="echoTextDisplay"
-        value={textMsg.join('\n')}
-      ></textarea>
+      <div className="displayBox">
+        <textarea
+          className="textDisplay"
+          id="echoTextDisplay"
+          value={textMsg.join('\n')}
+        ></textarea>
+      </div>
       <br></br>
-      <input
-        className="textInput"
-        id="echoText"
-        placeholder="what would you have me say?"
-        onChange={(e) => (message = e.target.value)}
-      ></input>
-      <button onClick={() => sendWebSocketMessage(socketState)}>Send</button>
-      <button onClick={() => sendWebSocketUndoMessage(socketState)}>
-        Undo
-      </button>
-      {/* <button onClick={() => setColor('blue')}>Blue</button> */}
-      {/* <button onClick={() => undoColor()}>Undo color</button> */}
-      {/* <span>{socketState}</span> */}
+      <div className="userInputs">
+        <input id="username" placeholder="Username"></input>
+        <input
+          className="textInput"
+          id="echoText"
+          placeholder="what would you have me say?"
+          onChange={(e) => (message = e.target.value)}
+        ></input>
+        <button onClick={() => sendWebSocketMessage(socketState)}>Send</button>
+        <button onClick={() => sendWebSocketUndoMessage(socketState)}>
+          Undo
+        </button>
+      </div>
     </div>
   );
 };
