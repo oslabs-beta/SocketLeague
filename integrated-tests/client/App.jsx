8e549139dfdd887db6fa436f553f93e87e637e17
samflam <me@samflam.com>
2022-01-15 14:35:15 -0800

    No longer passes React into the useSyncState hook

--- integrated-tests/client/App.jsx
+++ integrated-tests/client/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useSyncState } from 'socket-league-client';
 
 export default ({ conn }) => {
-  const [ number, setNumber, undoNumber ] = useSyncState('number', 0, conn, React);
+  const [ number, setNumber, undoNumber ] = useSyncState('number', 0, conn);
   return <>
     <span id="loaded">Page is loaded</span>
     <span id="number">{number}</span>
